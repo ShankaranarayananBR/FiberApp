cc92fd56692900cb3fd3ba984f870e141f4560e9
ShankaranarayananBR <brshankar1992@gmail.com>
2024-05-25 12:52:12 +0200

    Added an extra quote

--- model/cashiermodel.go
+++ model/cashiermodel.go
@@ -6,6 +6,6 @@ type Cashier struct {
 	Id        uint      `json:"id"`
 	Name      string    `json:"name"`
 	Passcode  string    `json:"passcode"`
-	CreatedAt time.Time `json:"created_at`
+	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
